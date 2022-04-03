23821d80fcfc80f288c91f354ff1b12e6909ddbe
Jakub Rogowski <jakerogowski33@gmail.com>
2022-04-03 14:09:36 +0200

    Drawing random movie title method added

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,54 @@
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     // Fields
-
+    public String randomTitle;
     // Constructor
 
     // Methods
-    void displayMovieList (String fileTitle) throws Exception {
+    /*void titleList (String fileTitle) throws Exception {
+        int lineCounter = 0;
         File file = new File (fileTitle);
         Scanner scanner = new Scanner(file);
 
         while (scanner.hasNextLine()) {
-            // Fields
             String movieTitle = scanner.nextLine();
             System.out.println(movieTitle);
         }
+    }*/
+
+    String movieDraw (String fileName) throws Exception {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+        List<String> listOfTitles = new ArrayList<>();
+        int lineCounter=0;
+
+        // Adding lines to the list
+        try (scanner) {
+            while (scanner.hasNextLine()) {
+                listOfTitles.add(scanner.nextLine());
+                lineCounter += 1;
+            }
+        }
+        // Declaring array that consists strings of movies' titles.
+        String [] arrayOfTitles = listOfTitles.toArray(new String[]{});
+        // Test output prints
+        System.out.println(Arrays.toString(arrayOfTitles));
+        for (int i=0; i<lineCounter; i++) {
+            System.out.println(arrayOfTitles[i]);
+        }
+
+        // Drawing a random movie title
+        int randomNumber = (int) (Math.random()*lineCounter);
+        System.out.println(randomNumber);
+        randomTitle = arrayOfTitles[randomNumber];
+        System.out.println(randomTitle);
+
+        return randomTitle;
     }
+
 }
--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -1,15 +1,10 @@
 public class GuessTheMovie {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Initializing GuessTheMovie game
         Game game = new Game();
-
-        // Displaying list of movies
-        try {
-            game.displayMovieList("movies.txt");
-        } catch (Exception exception) {
-            System.out.println("File missing!");
-        }
-
+        // Movie draw
+        game.movieDraw("movies.txt");
+        System.out.println(game.randomTitle);
 
     }
 }
