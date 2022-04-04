02e67717f77a0ae652abdd939d7c847abfadf298
Jakub Rogowski <jakerogowski33@gmail.com>
2022-04-04 19:31:05 +0200

    Code completed. Added neccessary methods and edited titleGuessAlgorithm

--- src/Game.java
+++ src/Game.java
@@ -6,6 +6,10 @@ import java.util.Scanner;
 public class Game {
     // Fields
     public String randomTitle;
+    public char letter;
+    public String hiddenTitle;
+    public int wrongAnswers = 0;
+    public String wrongLetters = "";
     // Constructor
 
     // Methods
@@ -32,29 +36,48 @@ public class Game {
         return randomTitle;
     }
 
-    void titleGuessAlgorithm () {
+    String hideTitle () {
         // Creating new string with letters converted to underscores
-        int numberOfLetters = randomTitle.length();
-        String hiddenTitle = randomTitle.replaceAll("(?i)[a-z]", "_"); // (?i) case insensitive
+        hiddenTitle = randomTitle.replaceAll("(?i)[a-z]", "_"); // (?i) case insensitive
         System.out.println("Hidden title: " + hiddenTitle);
+        return hiddenTitle;
+    }
 
+    char getLetterFromUser () {
         // User input
+        System.out.println("You are guessing: " + hiddenTitle);
+        System.out.println("You have guessed (" + wrongAnswers + ") wrong letters: " + wrongLetters);
         Scanner userInput = new Scanner(System.in);
-        System.out.println("Enter one letter: ");
-        char letter = userInput.next().charAt(0);
-        System.out.println("You chose letter: " + letter);
+        //System.out.println("Enter one letter: ");
+        letter = Character.toLowerCase(userInput.next().charAt(0));
+        //System.out.println("You chose letter: " + letter);
+        return letter;
+    }
 
+    String titleGuessAlgorithm () {
         // Getting index(es) of typed letter and checking if there's any
         int index;
-        if (randomTitle.indexOf(letter) != -1) {
+
+        if (randomTitle.indexOf(letter) != -1 && hiddenTitle.indexOf(letter) == -1) {
             index = randomTitle.indexOf(letter);
             while(index >= 0) {
-                System.out.println(index);
+                //System.out.println(index);
+                hiddenTitle = hiddenTitle.substring(0, index) + letter + hiddenTitle.substring(index + 1);
                 index = randomTitle.indexOf(letter, index+1);
             }
-        } else {
+        } else if (hiddenTitle.indexOf(letter) != -1) {
+            System.out.println("You already guessed that letter!");
+        }
+        else {
+            wrongAnswers += 1;
+            wrongLetters += letter + " ";
             System.out.println("There is no '" + letter + "' in that title!");
         }
+        return hiddenTitle;
+    }
+
+    boolean hasLost () {
+        return wrongAnswers == 10;
     }
 
 }
--- src/GuessTheMovie.java
+++ src/GuessTheMovie.java
@@ -2,10 +2,28 @@ public class GuessTheMovie {
     public static void main(String[] args) throws Exception {
         // Initializing GuessTheMovie game
         Game game = new Game();
+
         // Movie draw
         game.movieDraw("movies.txt");
         System.out.println("Random movie title is: " + game.randomTitle);
 
-        game.titleGuessAlgorithm();
+        // Hiding movie title by replacing its letters to '_'
+        game.hideTitle();
+
+        // The game itself
+        while (game.hiddenTitle.indexOf('_') != -1 && !game.hasLost()) {
+            game.getLetterFromUser();
+            game.titleGuessAlgorithm();
+            game.hasLost();
+        }
+
+        // Checking if player won the game
+        if (game.hasLost()) {
+            System.out.println("You have lost :(");
+            System.out.println("The movie was: " + game.randomTitle);
+        } else {
+            System.out.println("You win!");
+            System.out.println("You have guessed '" + game.randomTitle + "' correctly." );
+        }
     }
 }
